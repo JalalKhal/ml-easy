d07379851e5c3a89c3fffca370371431e7b45e9e
jalal <jalalkhaldi3@gmail.com>
2024-07-28 15:12:12 +0200

    update

--- recipe_template/test.py
+++ recipe_template/test.py
@@ -1,6 +1,9 @@
 from recipes.interfaces.recipe import Recipe
-from recipes.steps.cards_config import StepMessage
+from recipes.steps.steps_config import RecipePathsConfig
 
 if __name__ == '__main__':
-    recipe = Recipe("/home/khaldi/Documents/github_repos/refined_mlflow/recipe_template")
-    message: StepMessage = recipe.run()
+    paths_config = RecipePathsConfig(recipe_root_path = "/home/khaldi/Documents/github_repos/refined_mlflow/recipe_template", profile = "local")
+    recipe = Recipe(paths_config)
+
+
+    print(recipe.run())
