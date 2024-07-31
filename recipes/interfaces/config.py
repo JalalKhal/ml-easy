fff5985598118e4a502e5d7305d32c03dc7a2e1d
jalal <jalalkhaldi3@gmail.com>
2024-07-31 23:17:13 +0200

    update

--- recipes/interfaces/config.py
+++ recipes/interfaces/config.py
@@ -2,8 +2,6 @@ from typing import Union
 
 from pydantic import BaseModel
 
-from recipes.classification.v1.config import ClassificationRecipeConfig
-
 
 class BaseStepConfig(BaseModel):
     pass
@@ -12,15 +10,17 @@ class BaseStepConfig(BaseModel):
 class BaseStepsConfig(BaseModel):
     pass
 
+
 class Context(BaseModel):
     recipe_root_path: str
     target_col: str
 
+
 class BaseRecipeConfig(BaseModel):
-    steps: Union["ClassificationRecipeConfig"]
+    steps: Union['ClassificationStepsConfig']
     recipe: str
     context: Context
 
+
 class BaseCard(BaseModel):
     step_output_path: str
-
